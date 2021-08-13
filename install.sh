182145cae0cedbe7f375affc32838c7e21e31021
Sten Achiho <sten@achicoin.org>
2021-08-14 01:12:00 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git submodule update --init mozilla-ca
+
+echo "Ubuntu 20.04 LTS only supported."
+
+sudo apt-get update
+sudo apt-get install -y python3.8-venv python3-distutils
+
+ln -s venv/bin/activate .
+
+python -m pip install --upgrade pip
+python -m pip install wheel
+
+python -m pip install --extra-index-url https://pypi.achicoin.org/simple/ miniupnpc==2.2.2
+python -m pip install -e . --extra-index-url https://pypi.achicoin.org/simple/
+
+echo ""
+echo "Achi blockchain install.sh complete."
+echo ""
+echo "Type '. ./activate' and then 'ahia init' to begin."
